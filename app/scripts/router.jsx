abf532517f3d6732086e38673e3a33fdb3b226c4
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-10 08:28:44 -0700

    Add TODOs for handlers that must be made

--- app/scripts/router.jsx
+++ app/scripts/router.jsx
@@ -11,27 +11,39 @@ let routes = (
         <Route handler={Home} name="student-login" path="/student/login"/>
         <Route handler={Home} name="teacher-login" path="/teacher/login"/>
 
+        {/* TODO(neel): create student shell (simple header bar) */}
         <Route handler={Shell} name="student-layout" path="/student">
+            {/* TODO(neel): create class view (wrapper around module view) */}
             <DefaultRoute handler={Home} name="student-class"/>
         </Route>
 
         <Route handler={TeacherLayout} name="teacher-layout" path="/teacher">
+            {/* TODO(neel): create class wrapper (header with name of class) */}
             <Route handler={Shell} name="class" path="class/:classId">
+                {/* TODO(neel): module view (preview & student responses) */}
                 <Route handler={Home} name="running-module" path="modules/:moduleId"/>
+                {/* TODO(neel): class dashboard (list of modules & active one) */}
                 <DefaultRoute handler={Home} name="class-dashboard"/>
             </Route>
 
+            {/* TODO(neel): pretty much a thin wrapper */}
             <Route handler={Shell} name="teacher-modules" path="modules">
+                {/* TODO(neel): module preview and name (no student responses)  */}
                 <Route handler={Home} name="preview-module" path=":moduleId"/>
+                {/* TODO(neel): list of modules & searching */}
                 <DefaultRoute handler={Home} name="module-library"/>
             </Route>
 
+            {/* TODO(neel): class creation form  */}
             <Route handler={Home} name="create-class" path="create/class"/>
+            {/* TODO(neel): module creation form */}
             <Route handler={Home} name="create-module" path="create/module"/>
 
+            {/* TODO(neel): point this to student login */}
             <DefaultRoute handler={Home} name="teacher-dashboard"/>
         </Route>
 
+        {/* TODO(neel): point this to student login */}
         <DefaultRoute handler={Home}/>
         <NotFoundRoute handler={NotFound}/>
     </Route>
