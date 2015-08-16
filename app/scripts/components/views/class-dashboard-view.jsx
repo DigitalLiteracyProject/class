a2cdd64707abe1cf57afaed69d0e52f2848b6168
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-15 21:22:14 -0700

    Teacher class dashboard view

--- app/scripts/components/views/class-dashboard-view.jsx
+++ app/scripts/components/views/class-dashboard-view.jsx
@@ -1,21 +1,63 @@
 import React from 'react';
 import {ButtonLink} from 'react-router-bootstrap';
+import {Row, Col, Panel, Input, Button} from 'react-bootstrap';
 
 /**
- * Boilerplate for a "leaf" view -- one without sub-views.
+ * A teacher's home page when running a class. Contains a list of modules
+ * available to the teacher and information about the current module.
  */
-let HomeView = React.createClass({
+let ClassDashboardView = React.createClass({
 
     render: function() {
+        let searchButton = (
+            <Button>
+                Search
+            </Button>
+        );
 
         return (
-            <div className="hero-unit">
-                <h1>This is DLP Class!</h1>
-                <p>Not CS50.</p>
-                <ButtonLink to="class-module" params={{ classId: 1636, moduleId: 42 }}>Check out a module</ButtonLink>
-            </div>
+            <Row>
+                <Col xs={12} md={6}>
+                    <h2>"My Current Module"</h2>
+
+                    <Panel>
+                        <h3>Module Preview</h3>
+                    </Panel>
+
+                    <ButtonLink
+                            to="class-module"
+                            params={{ classId: 1636, moduleId: 42 }}
+                            bsStyle="primary"
+                            block>
+                        View student input
+                    </ButtonLink>
+                </Col>
+                <Col xs={12} md={6}>
+                    <h2>Choose a new module</h2>
+
+                    <Input type="text" buttonAfter={searchButton} />
+
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                A
+                            </Panel>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                B
+                            </Panel>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <Panel>
+                                C
+                            </Panel>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         );
     }
 });
 
-export default HomeView;
+export default ClassDashboardView;
