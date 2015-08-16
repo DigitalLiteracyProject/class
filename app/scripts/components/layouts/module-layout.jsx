4aae3dcfa8b5079b9c2a1f13aa10b5a4b82d40b8
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-15 21:38:02 -0700

    Module layout

--- app/scripts/components/layouts/module-layout.jsx
+++ app/scripts/components/layouts/module-layout.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import {RouteHandler} from 'react-router';
 
 /**
- * Boilerplate for a view that contains other views.
+ * Wraps all teacher module views, e.g. the teacher's module library.
  */
-let ShellLayout = React.createClass({
+let ModuleLayout = React.createClass({
 
     render: function() {
-
         return (
             <div>
                 <div className="container">
@@ -18,4 +17,4 @@ let ShellLayout = React.createClass({
     }
 });
 
-export default ShellLayout;
+export default ModuleLayout;
