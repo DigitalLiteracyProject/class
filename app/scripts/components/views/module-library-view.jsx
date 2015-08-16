b8bd5706e5c671a811ce9627e8732a49fa20496b
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-15 21:53:45 -0700

    Module library view

--- app/scripts/components/views/module-library-view.jsx
+++ app/scripts/components/views/module-library-view.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import {ButtonLink} from 'react-router-bootstrap';
+import {Row, Col, Panel, Input, Button} from 'react-bootstrap';
+
 /**
  * Boilerplate for a "leaf" view -- one without sub-views.
  */
 let HomeView = React.createClass({
 
     render: function() {
+        // TODO(neel): consider removing the search button in favor of
+        // auto-loading search results on key press
+        let searchButton = (
+            <Button>
+                Search
+            </Button>
+        );
 
         return (
-            <div className="hero-unit">
-                <h1>Modules -- Modules everywhere</h1>
-                <ButtonLink to="preview-module" params={{ moduleId: 95 }}>Check one out</ButtonLink>
+            <div>
+                <h2>Choose a new module</h2>
+
+                <Input type="text" buttonAfter={searchButton} />
+
+                <Row>
+                    <Col xs={6} md={4}>
+                        <Panel>
+                            {/* TODO(neel): STOPSHIP make this Panel a link
+                                in itself */}
+                            <ButtonLink
+                                    to="preview-module"
+                                    params={{ moduleId: 95 }}>
+                                First module
+                            </ButtonLink>
+                        </Panel>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Panel>
+                            B
+                        </Panel>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Panel>
+                            C
+                        </Panel>
+                    </Col>
+                </Row>
             </div>
         );
     }
