09f1301271f9231eac921897bf7cd42d8dd11bc3
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-15 21:32:42 -0700

    Class module view

--- app/scripts/components/views/class-module-view.jsx
+++ app/scripts/components/views/class-module-view.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
+import {Row, Col, Panel} from 'react-bootstrap';
 
 /**
- * Boilerplate for a "leaf" view -- one without sub-views.
+ * When a module is being used in a class, shows a preview of it and
+ * module-specific student data.
  */
-let HomeView = React.createClass({
+let ClassModuleView = React.createClass({
 
     render: function() {
-
         return (
-            <div className="hero-unit">
-                <h1>This is DLP Class!</h1>
-                <p>Cool module, bro!</p>
+            <div>
+                <h2>"My Cool Module"</h2>
+
+                <Row>
+                    <Col xs={12} md={6}>
+                        <h3>Module Preview</h3>
+
+                        {/* TODO(neel): extract this out to its own component
+                            (the same preview appears in class dashboard view)
+                            */}
+                        <Panel>
+                            <h3>Module Preview</h3>
+                        </Panel>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <h3>Student Data</h3>
+
+                        <Row>
+                            <Col xs={6} md={4}>
+                                <Panel>
+                                    A
+                                </Panel>
+                            </Col>
+                            <Col xs={6} md={4}>
+                                <Panel>
+                                    B
+                                </Panel>
+                            </Col>
+                            <Col xs={6} md={4}>
+                                <Panel>
+                                    C
+                                </Panel>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             </div>
         );
     }
 });
 
-export default HomeView;
+export default ClassModuleView;
