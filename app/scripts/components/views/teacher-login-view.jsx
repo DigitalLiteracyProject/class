886b9bf4e5ea13c10d091415568a0a9a9e339fd7
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-15 19:48:52 -0700

    Improve code formatting

--- app/scripts/components/views/teacher-login-view.jsx
+++ app/scripts/components/views/teacher-login-view.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {ButtonLink} from 'react-router-bootstrap';
-import {Input} from 'react-bootstrap';
 
 /**
  * Renders a page for teachers to log in.
@@ -8,9 +7,11 @@ import {Input} from 'react-bootstrap';
 let TeacherLoginView = React.createClass({
 
     render: function() {
-
         return (
-            <ButtonLink to="teacher-dashboard" bsStyle="primary" bsSize="large"
+            <ButtonLink
+                    to="teacher-dashboard"
+                    bsStyle="primary"
+                    bsSize="large"
                     block>
                 Log in with Google
             </ButtonLink>
