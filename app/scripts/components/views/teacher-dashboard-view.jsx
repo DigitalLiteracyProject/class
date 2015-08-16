d4fe977feaedde36017e6dedf8bd2c41ad3ce37e
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-15 20:16:31 -0700

    Teacher dashboard

--- app/scripts/components/views/teacher-dashboard-view.jsx
+++ app/scripts/components/views/teacher-dashboard-view.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import {ButtonLink} from 'react-router-bootstrap';
+import {PageHeader, ListGroup, ListGroupItem} from 'react-bootstrap';
 
 /**
- * Boilerplate for a "leaf" view -- one without sub-views.
+ * Shown to the teacher upon logging in; contains a list of classes.
  */
-let HomeView = React.createClass({
+let TeacherDashboardView = React.createClass({
 
     render: function() {
-
         return (
             <div className="hero-unit">
-                <h1>Sup, teacher!</h1>
-                <ButtonLink to="class-dashboard" params={{ classId: 1636 }}>Go teach</ButtonLink>
+                <PageHeader>
+                    Hi, <strong>Neel</strong>.
+                </PageHeader>
+
+                <ListGroup>
+                  <ListGroupItem>
+                    {/* TODO(neel): STOPSHIP use React Router Bootstrap to get a
+                        linked item */}
+                        <ButtonLink to="class-dashboard"
+                                params={{ classId: 1636 }}>
+                            Cool School Fall 2015
+                        </ButtonLink>
+                  </ListGroupItem>
+                </ListGroup>
             </div>
         );
     }
 });
 
-export default HomeView;
+export default TeacherDashboardView;
