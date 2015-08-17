d1fc680aaaa25235c13c6448d7309131ab3f1b1a
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-17 01:32:57 -0400

    Add and start testing socket.io

--- app/scripts/components/layouts/top-layout.jsx
+++ app/scripts/components/layouts/top-layout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
+import io from 'socket.io-client';
 
 /**
  * Top-level wrapper component; all other views are contained within this.
  */
 let TopLayout = React.createClass({
+
+    componentDidMount: function() {
+        let socket = io();
+        // TODO(neel): test this here
+    },
 
     render: function() {
         return (
--- gulpfile.js/tasks/server.js
+++ gulpfile.js/tasks/server.js
@@ -5,15 +5,28 @@ var gutil    = require('gulp-util');
 var compress = require('compression');
 var logger   = require('morgan');
 var open     = require('open');
+var http     = require('http');
+var io       = require('socket.io');
 
 gulp.task('server', function() {
   var url = 'http://localhost:' + config.port;
 
-  express()
+  var app = express();
+
+  // set up app
+  app
     .use(compress())
     .use(logger(config.logLevel))
     .use('/', express.static(config.root, config.staticOptions))
     .listen(config.port)
+
+  // set up socket.io
+  io(http.Server(app))
+    .on('connection', function(socket){
+      console.log('a user connected');
+    });
+    // TODO(neel): test this here
+
 
   gutil.log('production server started on ' + gutil.colors.green(url));
   open(url);
