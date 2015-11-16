3bdd8c38c78b66e22e4367a25f27873587c14453
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2015-11-16 17:19:38 -0500

    set interval to updating time continuously

--- app/scripts/components/views/teacher-time-view.jsx
+++ app/scripts/components/views/teacher-time-view.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 let TeacherTimeView = React.createClass({
-  render: function(){
-    var date = new Date();
-    //
-    return (
-      <p>{date.getHours()}:{date.getMinutes()}:{date.getSeconds()}</p>
-    );
-  }
+    getInitialState: function(){
+	return {
+	    date: new Date()
+	};
+    },
+    componentDidMount: function(){
+	var that = this;
+	setInterval(function(){
+	    that.setState({date: new Date()});
+	}, 1000);
+    },
+    render: function(){
+	return (
+	    <p>{this.state.date.getHours()}:{this.state.date.getMinutes()}:{this.state.date.getSeconds()}</p>
+	);
+    }
 });
 
 export default TeacherTimeView;
