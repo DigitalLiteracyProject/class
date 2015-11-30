96ef4bf8ef691a52879e1f64730d6166a0b02c65
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2015-11-30 16:23:53 -0500

    fixed indents, stopped showing seconds on timeview, used arrow function instead of this/that

--- app/scripts/components/views/teacher-time-view.jsx
+++ app/scripts/components/views/teacher-time-view.jsx
@@ -3,24 +3,22 @@ import {ProgressBar} from 'react-bootstrap'
 
 let TeacherTimeView = React.createClass({
     getInitialState: function(){
-	return {
-	    date: new Date()
-	};
+        return {
+            date: new Date()
+        };
     },
     componentDidMount: function(){
-	var that = this; //work-around for scope issue
-	setInterval(function(){
-	    that.setState({date: new Date()});
-	}, 1000);
+        setInterval(() => {
+            this.setState({date: new Date()});
+        }, 1000);
     },
     render: function(){
-	var label = this.state.date.getHours()%12
-	label += ':'+('00'+this.state.date.getMinutes()).slice(-2);
-	label += ':'+('00'+this.state.date.getSeconds()).slice(-2);
-	var now = this.state.date.getMinutes()+(this.state.date.getSeconds()/60.0);
-	return (
-	    <div><ProgressBar min={0} max={60} now={now} label={label} /></div> // placeholders assume class starts and ends on the hour
-	);
+        var label = this.state.date.getHours()%12
+        label += ':'+('00'+this.state.date.getMinutes()).slice(-2);
+        var now = this.state.date.getMinutes()+(this.state.date.getSeconds()/60.0);
+        return (
+            <div><ProgressBar min={0} max={60} now={now} label={label} /></div> // placeholders assume class starts and ends on the hour
+        );
     }
 });
 
